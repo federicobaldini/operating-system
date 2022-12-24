b091aa192110fe066698b3338f0f10e116ea887c
Federico Baldini <federico.baldini7@gmail.com>
2022-12-24 10:47:14 +0100

    Update main.rs
    Used the module vga_buffer.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,14 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
+mod vga_buffer;
 use core::panic::PanicInfo;
 
 static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-  let vga_buffer = 0xb8000 as *mut u8;
-
-  for (i, &byte) in HELLO.iter().enumerate() {
-    unsafe {
-      *vga_buffer.offset(i as isize * 2) = byte;
-      *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-    }
-  }
-
+  vga_buffer::print_something();
   loop {}
 }
 
