280885ffdf4525a91e15f575cf8807039dbb39e5
Federico Baldini <federico.baldini7@gmail.com>
2022-12-24 10:45:56 +0100

    Update main.rs
    Removed comments.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-// src/main.rs
-
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
@@ -9,22 +7,6 @@ static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-  /*
-     For printing “Hello World!”, we just need to know that the buffer is located
-     at address 0xb8000 and that each character cell consists of
-     an ASCII byte and a color byte.
-    
-    First, we cast the integer 0xb8000 into a raw pointer. Then we iterate over the bytes
-    of the static HELLO byte string. We use the enumerate method to additionally get a
-    running variable i. In the body of the for loop, we use the offset method to write the
-    string byte and the corresponding color byte (0xb is a light cyan).
-
-    Note that there’s an unsafe block around all memory writes. The reason is that the
-    Rust compiler can’t prove that the raw pointers we create are valid. They could
-    point anywhere and lead to data corruption. By putting them into an unsafe block,
-    we’re basically telling the compiler that we are absolutely sure that the
-    operations are valid. Note that an unsafe block does not turn off Rust’s safety checks.
-    */
   let vga_buffer = 0xb8000 as *mut u8;
 
   for (i, &byte) in HELLO.iter().enumerate() {
