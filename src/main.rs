a1087a976ef2031affbf36961e89742391ac1a70
Federico Baldini <federico.baldini7@gmail.com>
2022-12-27 11:22:54 +0100

    Update main.rs
    Implemented a custom test framework for the kernel.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
-#![no_std] // don't link the Rust standard library
-#![no_main] // disable all Rust-level entry points
+#![no_std] // Don't link the Rust standard library
+#![no_main] // Disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
 use core::panic::PanicInfo;
 
-#[no_mangle] // don't mangle the name of this function
+#[no_mangle] // Don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
   println!("Hello World{}", "!");
+
+  #[cfg(test)]
+  test_main();
+
   loop {}
 }
 
@@ -15,4 +22,19 @@ pub extern "C" fn _start() -> ! {
 fn panic(info: &PanicInfo) -> ! {
   println!("{}", info);
   loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+  println!("Running {} tests", tests.len());
+  for test in tests {
+    test();
+  }
+}
+
+#[test_case]
+fn trivial_assertion() {
+  print!("trivial assertion... ");
+  assert_eq!(1, 1);
+  println!("[ok]");
 }
