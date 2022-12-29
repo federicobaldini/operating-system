f7df082c66c18bf60675a6d9bedd51fd6f6a311b
Federico Baldini <federico.baldini7@gmail.com>
2022-12-29 13:26:08 +0100

    Update main.rs
    Automatically close QEMU after run "cargo test".

--- src/main.rs
+++ src/main.rs
@@ -24,12 +24,28 @@ fn panic(info: &PanicInfo) -> ! {
   loop {}
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+  Success = 0x10,
+  Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+  use x86_64::instructions::port::Port;
+  unsafe {
+    let mut port = Port::new(0xf4);
+    port.write(exit_code as u32);
+  }
+}
+
 #[cfg(test)]
 fn test_runner(tests: &[&dyn Fn()]) {
   println!("Running {} tests", tests.len());
   for test in tests {
     test();
   }
+  exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
