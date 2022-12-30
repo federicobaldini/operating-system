701fd0524eea1cd1a477dbdf006af048e29cdc9d
Federico Baldini <federico.baldini7@gmail.com>
2022-12-30 11:48:28 +0100

    Update main.rs
    Used the serial_print() method.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+mod serial;
 mod vga_buffer;
 use core::panic::PanicInfo;
 
@@ -41,7 +42,7 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
 
 #[cfg(test)]
 fn test_runner(tests: &[&dyn Fn()]) {
-  println!("Running {} tests", tests.len());
+  serial_println!("Running {} tests", tests.len());
   for test in tests {
     test();
   }
@@ -50,7 +51,7 @@ fn test_runner(tests: &[&dyn Fn()]) {
 
 #[test_case]
 fn trivial_assertion() {
-  print!("trivial assertion... ");
-  assert_eq!(1, 1);
-  println!("[ok]");
+  serial_print!("trivial assertion... ");
+  assert_eq!(0, 1);
+  serial_println!("[ok]");
 }
