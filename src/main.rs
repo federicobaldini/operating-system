d59a9bebcb9e92c6414811e21eadc8680e88e689
Federico Baldini <federico.baldini7@gmail.com>
2022-12-31 13:27:11 +0100

    Update main.rs
    Tests management moved from main.rs to lib.rs.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
-#![no_std] // Don't link the Rust standard library
-#![no_main] // Disable all Rust-level entry points
+#![no_std]
+#![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(operating_system::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-mod serial;
-mod vga_buffer;
+use operating_system::println;
 use core::panic::PanicInfo;
 
-#[no_mangle] // Don't mangle the name of this function
+#[no_mangle]
 pub extern "C" fn _start() -> ! {
   println!("Hello World{}", "!");
 
@@ -18,7 +17,6 @@ pub extern "C" fn _start() -> ! {
   loop {}
 }
 
-// This function is called on panic.
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
@@ -26,43 +24,8 @@ fn panic(info: &PanicInfo) -> ! {
   loop {}
 }
 
-// This function is called on panic in test mode.
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-  serial_println!("[failed]\n");
-  serial_println!("Error: {}\n", info);
-  exit_qemu(QemuExitCode::Failed);
-  loop {}
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-  Success = 0x10,
-  Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-  use x86_64::instructions::port::Port;
-  unsafe {
-    let mut port = Port::new(0xf4);
-    port.write(exit_code as u32);
-  }
-}
-
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-  serial_println!("Running {} tests", tests.len());
-  for test in tests {
-    test();
-  }
-  exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
-fn trivial_assertion() {
-  serial_print!("trivial assertion... ");
-  assert_eq!(0, 1);
-  serial_println!("[ok]");
+  operating_system::test_panic_handler(info)
 }
