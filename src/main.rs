fa8eaaa5d0c836e7317c5faca6652e68074a61a1
Federico Baldini <federico.baldini7@gmail.com>
2023-01-02 12:28:24 +0100

    Manage CPU exceptions
    Set up an interrupt descriptor table that provides handler functions, now the kernel will be able to catch breakpoint exceptions and resume normal execution afterward.

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,22 @@
 #![test_runner(operating_system::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use operating_system::println;
 use core::panic::PanicInfo;
+use operating_system::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
   println!("Hello World{}", "!");
 
+  operating_system::init();
+
+  // invoke a breakpoint exception
+  x86_64::instructions::interrupts::int3();
+
   #[cfg(test)]
   test_main();
 
+  println!("It did not crash!");
   loop {}
 }
 
